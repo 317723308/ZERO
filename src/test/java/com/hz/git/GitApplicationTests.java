12b34a3bb788a527ec0905edc9c04efbcde83321
317723308 <317723308@qq.com>
2022-04-25 11:06:27 +0800

    上传

---
+++ src/test/java/com/hz/git/GitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hz.git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
